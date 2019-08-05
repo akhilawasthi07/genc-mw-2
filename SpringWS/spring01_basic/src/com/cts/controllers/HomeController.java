7be0ba4c4c4846a4e9a574c450b28c21ab41b12a
Arun Kumar <java.kumar.arun@gmail.com>
2019-08-05 11:21:23 +0530

    spring mvc

---
+++ SpringWS/spring01_basic/src/com/cts/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.cts.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("greet/{name}")
+	public ModelAndView sayHello(@PathVariable String name) {
+		ModelAndView mv = new ModelAndView("hello");
+		System.out.println("Welcome "+name);
+		mv.addObject("msg", "Welcome "+name+"...");
+		return mv;
+	}
+}
